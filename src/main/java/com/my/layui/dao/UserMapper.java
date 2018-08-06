a7ffa3001a3b3d978573a44daadcde6349cb7ae7
YiChuan <liufanfandev@163.com>
2018-08-06 17:31:32 +0800

    mybitis generate

---
+++ src/main/java/com/my/layui/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.my.layui.dao;
+
+import com.my.layui.model.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/my/layui/model/User.java
@@ -0,0 +1,43 @@
+package com.my.layui.model;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String sex;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
