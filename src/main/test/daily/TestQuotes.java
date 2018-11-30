fec4eb1911ded2eb6107d1674e26486dfca944bc
YiChuan <liufanfandev@163.com>
2018-11-30 17:08:27 +0800

    同步并发代码

--- src/main/test/daily/TestQuotes.java
+++ src/main/test/daily/TestQuotes.java
@@ -7,9 +7,11 @@ public class TestQuotes {
 	public static final String SERVICEGROUPADD = "resource group_service add name \"{0}\" member \"{1}\" '{' desc \"{2}\" '}'";
 	public static final String ITEM = "add item name  ''{0}'' '{' value ''{1}'' '}' ";
 	public static final String DELETEADDRESSGROUP = "resource group_addr delete { name ''{0}'' }";
+	public static final String INTERAUTONEG = "network interface autoneg  [''{0}'']";
+	
 	public static void main(String[] args) {
 		Object[] params = {"555"};
-		String msg= MessageFormat.format(DELETEADDRESSGROUP, params);
+		String msg= MessageFormat.format(INTERAUTONEG, params);
 		System.out.println(msg);
 	
 	}
--- src/main/test/thinking/concurrent/SimplePriorities.java
+++ src/main/test/thinking/concurrent/SimplePriorities.java
@@ -4,13 +4,20 @@ public class SimplePriorities implements Runnable{
 	private int countDown = 5;
 	private volatile double d;
 	private int priority;
-	public SimplePriorities() {
-		
+	public SimplePriorities(int priority) {
+		this.priority = priority;
+	}
+	@Override
+	public String toString() {
+		return Thread.currentThread()+":"+countDown;
 	}
 
 	@Override
 	public void run() {
-		
+		Thread.currentThread().setPriority(priority);
+		while(true){
+			
+		}
 	}
 
 }
